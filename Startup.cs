6024d8657404445c8d1dd520bdd11408141e78ab
Perttu <perttu.leppanen@edu.savonia.fi>
2021-09-12 14:38:55 +0300

    Message

--- Startup.cs
+++ Startup.cs
@@ -26,10 +26,6 @@ namespace BooManagerApi
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BooManagerApi", Version = "v1" });
-            });
 
             services.AddCors(options =>
             {
@@ -47,7 +43,6 @@ namespace BooManagerApi
 
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseCors(MyAllowSpecificOrigins);
@@ -55,8 +50,6 @@ namespace BooManagerApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BooManagerApi v1"));
             }
 
             app.UseHttpsRedirection();
